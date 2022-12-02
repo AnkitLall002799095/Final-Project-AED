858980ee7a3b2e611e88adca1ad9fb25507a7edc
Harshit Mittal <mittal.h@northeastern.edu>
2022-12-01 23:19:29 -0500

    Working on Apartment listing view.

--- FinalProjectAED/src/Application/Utils/Helper.java
+++ FinalProjectAED/src/Application/Utils/Helper.java
@@ -4,10 +4,25 @@
  */
 package Application.Utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Bhawna Bassarmalani <your.name at your.org>
  */
 public class Helper {
+    
+    public static Connection getConnection(){
+        try{
+            Connection conn;
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://aeddatabase.cvxm5l9d0hm0.us-east-1.rds.amazonaws.com:3306/aedfinalproject", "admin", "password");
+            return conn;
+        }
+        catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
     
 }
