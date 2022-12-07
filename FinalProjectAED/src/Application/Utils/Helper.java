543a6b2c3c24dfae9665908684783bfbec74f7c1
ankitlall <ankitlall@localhost>
2022-12-07 13:22:26 -0500

    copy image

--- FinalProjectAED/src/Application/Utils/Helper.java
+++ FinalProjectAED/src/Application/Utils/Helper.java
@@ -4,10 +4,46 @@
  */
 package Application.Utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import org.apache.commons.io.FileUtils;
+
 /**
  *
  * @author Bhawna Bassarmalani <your.name at your.org>
  */
 public class Helper {
+    
+    
+//    public static void copyFile(File sourceFile, File destFile) {
+    public static String copyFile(String filePath) {
+        String relFilePath = "";
+        try{
+            //absolute path for source file to be copied
+            String source = filePath;
+            //directory where file will be copied
+            String target ="./assets/";
+
+            //name of source file
+            File sourceFile = new File(source);
+            String name = sourceFile.getName();
+
+            File targetFile = new File(target+name);
+            System.out.println("Copying file : " + sourceFile.getName() +" from Java Program");
+
+            //copy file from one location to other
+            FileUtils.copyFile(sourceFile, targetFile);
+
+            System.out.println("copying of file from Java program is completed"); 
+            relFilePath = target.concat(sourceFile.getName());
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+        
+        return relFilePath;
+    }
     
 }
